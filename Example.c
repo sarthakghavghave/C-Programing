071ea2c2f120a285d8b26ef4ac79730ad99aa299
sarthakghavghave <sarthak.ghavghave@gmail.com>
2023-10-03 21:53:01 +0530

    1st commit

---
+++ Example.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    //input:ABCD  output:DCBA
+    int s;
+    char a[100];
+    printf ("Enter the size of string: ");
+    scanf ("%d",&s);
+
+    printf("Enter the string: ");
+    scanf ("%d%d%d%d");
+
+
+}
